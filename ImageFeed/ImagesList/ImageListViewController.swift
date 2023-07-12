ddd996ec4cb75417611515420a97dffa3d805162
Eldar Aidumov <superaydumov@me.com>
2023-07-12 23:24:06 +0300

    Refactored ImageListViewController and ImageListCell classes.

--- ImageFeed/ImagesList/ImageListViewController.swift
+++ ImageFeed/ImagesList/ImageListViewController.swift
@@ -39,38 +39,6 @@ class ImagesListViewController: UIViewController {
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 }
-
-extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        gradientViewSet(cell)
-        
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return
-        }
-
-        cell.cellImage.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
-
-        let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "like button ON") : UIImage(named: "like button OFF")
-        cell.likeButton.setImage(likeImage, for: .normal)
-    }
-    
-    func gradientViewSet(_ cell: ImagesListCell) {
-        let gradientViewLayer = CAGradientLayer()
-        let topColor = UIColor(red: 0.27, green: 0.26, blue: 0.34, alpha: 0.00)
-        let bottomColor = UIColor(red: 0.27, green: 0.26, blue: 0.34, alpha: 0.20)
-        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
-        
-        gradientViewLayer.frame = cell.gradientView.bounds
-        gradientViewLayer.colors = gradientColors
-        
-        cell.gradientView.backgroundColor = UIColor.clear
-        cell.gradientView.layer.insertSublayer(gradientViewLayer, at: 0)
-        cell.gradientView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-    }
-}
-
     // MARK: - UITableViewDataSource
 
 extension ImagesListViewController: UITableViewDataSource {
@@ -79,22 +47,24 @@ extension ImagesListViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) as? ImagesListCell else { return UITableViewCell() }
 
-        guard let imageListCell = cell as? ImagesListCell else {
-            return UITableViewCell()
-        }
+        let image = UIImage(named: photosName[indexPath.row])
+        let date = dateFormatter.string(from: Date())
+        let isLiked = indexPath.row % 2 == 0
+        
+        cell.configureCell(image: image, date: date, isLiked: isLiked)
 
-        configCell(for: imageListCell, with: indexPath)
-
-        return imageListCell
+        return cell
     }
 }
 
     // MARK: - UITableViewDelegate
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
