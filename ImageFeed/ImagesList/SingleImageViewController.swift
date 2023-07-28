2d4f3c71277225b56d02a1007d33f030084e25d5
Eldar Aidumov <superaydumov@me.com>
2023-07-28 23:52:31 +0300

    Added ability to zoom and share selected photo. Refactored ProfileViewController (all UI elements added through code).

--- ImageFeed/ImagesList/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -8,17 +8,65 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    
+    // MARK: - Properties
     var image: UIImage! {
         didSet {
             guard isViewLoaded else { return }
             singleImageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
-    
+    // MARK: - IBOutlets
     @IBOutlet weak var singleImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         singleImageView.image = image
+        
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+    
+    // MARK: - Private functions
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: Any) {
+        let share = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(share, animated: true)
+    }
+    
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        singleImageView
     }
 }
