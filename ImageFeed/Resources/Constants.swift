d4fd2d42347474be1c9f19af2efadec4aa075e99
Eldar Aidumov <superaydumov@me.com>
2023-08-01 00:18:54 +0300

    Created folders for view controllers. Added "Constants" to the project (Unsplash keys, redirect information, api URL etc).

---
+++ ImageFeed/Resources/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Эльдар Айдумов on 01.08.2023.
+//
+
+import Foundation
+
+struct Constants {
+    let accessKey = "5cknYblskv1f9HgymUwDSz7X5dGqM9-0f1mpmVzebqU"
+    let secretLey = "CH3eNm3fTDWw60be512fQT8usbv6Gfw4Z7jsYnJv9z8"
+    let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    
+    let accessScope = "public+read_user+write_likes"
+    let defaultbaseURL = URL (string: "https://api.unsplash.com/")
+}
