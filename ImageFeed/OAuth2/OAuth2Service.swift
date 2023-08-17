b5d73b0db14463aa551b0924b8e060fb7545f940
Eldar Aidumov <superaydumov@me.com>
2023-08-17 13:42:11 +0300

    Refactored code (deleted additional property in switch success case).

--- ImageFeed/OAuth2/OAuth2Service.swift
+++ ImageFeed/OAuth2/OAuth2Service.swift
@@ -34,9 +34,8 @@ final class OAuth2Service {
             guard let self = self else { return }
             switch result {
             case .success (let body):
-                let authToken = body.accessToken
-                self.authToken = authToken
-                completion(.success(authToken))
+                self.authToken = body.accessToken
+                completion(.success(body.accessToken))
             case .failure(let error):
                 completion(.failure(error))
             }
